d8149576d087e41c8bedcddf00a47187d5f921a8
Fariane <123965127+Fariane@users.noreply.github.com>
2024-12-19 10:17:03 +0100

    Update spotify_api_data_extract.py

--- spotify_api_data_extract.py
+++ spotify_api_data_extract.py
@@ -1,3 +1,4 @@
+# Importing necessary libraries
 import json
 import os
 import spotipy
@@ -7,16 +8,21 @@ from datetime import datetime
 
 def lambda_handler(event, context):
 
+    # getting spotify credentials
     client_id = os.environ.get('client_id')
     client_secret = os.environ.get('client_secret')
 
+    # add your keys here
     client_credentials_manager = SpotifyClientCredentials(client_id = client_id  , client_secret = client_secret)
+
+    # Initializes a Spotify client using Spotipy with a client credentials manager for API authentication
     sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
     playlists = sp.user_playlists('spotipy')
 
     playlist_link = "https://open.spotify.com/playlist/5ABHKGoOzxkaa28ttQV9sE"
     playlist_URL = playlist_link.split("/")[-1]
 
+    # Fetch data from the spotipy
     spotipy_data = sp.playlist_tracks(playlist_URL)
 
     client = boto3.client('s3')
