82c83772d52e7502d80b5adff9b4110a08ae2bda
Fariane <123965127+Fariane@users.noreply.github.com>
2024-12-19 12:29:16 +0100

    Update spotify_api_data_extract.py

--- spotify_api_data_extract.py
+++ spotify_api_data_extract.py
@@ -8,27 +8,29 @@ from datetime import datetime
 
 def lambda_handler(event, context):
 
-    # getting spotify credentials
+    # Fetching Spotify credentials from environment variables
     client_id = os.environ.get('client_id')
     client_secret = os.environ.get('client_secret')
 
-    # add your keys here
+    # Initializing Spotify client credentials manager
     client_credentials_manager = SpotifyClientCredentials(client_id = client_id  , client_secret = client_secret)
 
-    # Initializes a Spotify client using Spotipy with a client credentials manager for API authentication
+    # Instantiating Spotipy client
     sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
     playlists = sp.user_playlists('spotipy')
 
+    # Spotify playlist link and extraction
     playlist_link = "https://open.spotify.com/playlist/5ABHKGoOzxkaa28ttQV9sE"
-    playlist_URL = playlist_link.split("/")[-1]
+    playlist_URL = playlist_link.split("/")[-1]  # Extract playlist ID from URL
+    spotipy_data = sp.playlist_tracks(playlist_URL)  # Fetch playlist data
 
-    # Fetch data from the spotipy
-    spotipy_data = sp.playlist_tracks(playlist_URL)
-
+    # Initializing AWS S3 client
     client = boto3.client('s3')
 
+    # Generating filename with timestamp
     filename = "spotipy_raw" + str(datetime.now()) + ".json"
 
+    # Uploading the playlist data to S3
     client.put_object( 
         Bucket = "spotify-etl-projet-fariane",
         Key = "raw_data/to_processed/" + filename,
